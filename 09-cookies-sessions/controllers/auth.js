37b7008860da07c9ac4e35dfcfdba4dfa0ffcd73
Om Patel <om@advant.xyz>
2026-05-29 17:50:29 +0530

    Move SendGrid key to environment variables

--- 09-cookies-sessions/controllers/auth.js
+++ 09-cookies-sessions/controllers/auth.js
@@ -1,6 +1,8 @@
+const user = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
     // const isLoggedIn = req.get('cookie').split('=')[1];
-    console.log(req.session.isLoggedIn);
+    // console.log(req.session.isLoggedIn);
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
@@ -9,8 +11,29 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-    // res.setHeader('Set-Cookie', 'loggedIn=true ; httpOnly');  //this is how you can set the cookie and httpOnly make this cookie safe so no one can edit the cookie value from the browser side 
-    // now let's see how session works 
-    req.session.isLoggedIn = true ;
-    res.redirect('/');
+    user.findById('6a181832cdc0c5b974678d9f')
+        .then(user => {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            req.session.save(err => {
+                console.log(err);
+                res.redirect('/');
+            });
+        })
+        .catch(err => console.log(err));
+};
+
+// exports.postLogin = (req, res, next) => {
+//     // res.setHeader('Set-Cookie', 'loggedIn=true ; httpOnly');  //this is how you can set the cookie and httpOnly make this cookie safe so no one can edit the cookie value from the browser side 
+//     // now let's see how session works 
+    
+//     req.session.isLoggedIn = true ;
+//     res.redirect('/');
+// };
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        console.log(err);
+        res.redirect('/');
+    });
 };
--- 10-authentication/app.js
+++ 10-authentication/app.js
@@ -4,18 +4,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
-const MongoDbStore = require('connect-mongodb-session')(session);
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
-const MONGO_URI = 'mongodb+srv://om_db_user:rSCJ8FXDWMgTfMxy@cluster0.5ntp767.mongodb.net/shop' ;
+const MONGODB_URI = 'mongodb+srv://om_db_user:rSCJ8FXDWMgTfMxy@cluster0.5ntp767.mongodb.net/shop';
 
 const app = express();
-const store = new MongoDbStore({
-  uri : MONGO_URI ,
-  collection : 'session'
-})
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'session'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -27,13 +30,15 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({ 
-    secret: 'my secret', 
-    resave: false, 
-    saveUninitialized: false ,
-    store : store ,
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
   })
 );
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -47,6 +52,12 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res ,next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
@@ -54,26 +65,8 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    MONGO_URI,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Om',
-          email: 'om@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch(err => {
